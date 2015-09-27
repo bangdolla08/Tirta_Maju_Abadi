8ba1c9defefd0779a8792152a4ca88dc2e13c6e9
NEEZAR <NEEZAR@NEEZAR-PC>
2015-09-27 12:02:07 +0700

    dul cobak koreksi PO_penjualan

--- src/Tirta_Maju_Abadi/View/Acc_bo.java
+++ src/Tirta_Maju_Abadi/View/Acc_bo.java
@@ -102,10 +102,6 @@ public class Acc_bo extends javax.swing.JInternalFrame {
             }
         });
 
-        modelTextFilt1.setText("modelTextFilt1");
-
-        modelTextFilt2.setText("modelTextFilt2");
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
